97d27320107abf3e2d40658d882808b7a2c37e8d
Michelle Kiss <picturethis56@gmail.com>
2019-04-25 16:11:34 -0500

    fixed our bug in index.js with page object

--- models/index.js
+++ models/index.js
@@ -4,16 +4,36 @@ const db = new Sequelize('postgres://localhost:5432/wikistack', {
 });
 
 const Page = db.define('page', {
-  title: {type: Sequelize.STRING} ,
-  slug: {type: Sequelize.STRING},
-  content: {type: Sequelize.TEXT},
-  status: {type: Sequelize.ENUM('open','closed')}
-})
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM('open','closed')
+  }
+});
 
 const User= db.define('user', {
-  name: {type: Sequelize.STRING},
-  email: {type: Sequelize.STRING}
-})
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+    // validate: {
+    //   isEmail: true
+    // }
+  }
+});
 
 db.authenticate().
 then(() => {
